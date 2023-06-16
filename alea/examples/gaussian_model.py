f3d240c09cdc4696d77246a078248beb60a52966
Knut Dundas Moraa <knutdundasmoraa@gmail.com>
2023-06-16 11:54:32 -0400

    Move make_objective and fit function to parent class, change some return errors to raise errors (mea culpa) made the dataset be an array of datasets to have the same format as the final nT likelihood (for storing and loading etc)

--- alea/examples/gaussian_model.py
+++ alea/examples/gaussian_model.py
@@ -6,10 +6,16 @@ from iminuit.util import make_func_code
 
 
 class GaussianModel(StatisticalModel):
-    def __init__(self, mu, sigma):
-        self.nominal_values = {"mu": mu,
-                               "sigma": sigma}
-        super().__init__(fixed_parameters={"sigma": sigma},
+    def __init__(self, nominal_mu, nominal_sigma):
+        """
+        Initialise a model of a gaussian measurement (hatmu),
+        where the model has parameters mu and sigma
+        For illustration, we show how required nominal parameters can be added to the init
+        sigma is fixed in this example.
+        """
+        self.nominal_values = {"mu": nominal_mu,
+                               "sigma": nominal_sigma}
+        super().__init__(fixed_parameters={"sigma": nominal_sigma},
                          )
 
     def ll(self, mu=None, sigma=None):
@@ -18,7 +24,7 @@ class GaussianModel(StatisticalModel):
         if sigma is None:
             sigma = self.nominal_values.get("sigma", None)
 
-        hat_mu = self.get_data()['hat_mu'][0]
+        hat_mu = self.get_data()[0]['hat_mu'][0]
         return stats.norm.logpdf(x=hat_mu, loc=mu, scale=sigma)
 
     def generate_data(self, mu=None, sigma=None):
@@ -28,73 +34,6 @@ class GaussianModel(StatisticalModel):
             sigma = self.nominal_values.get("sigma", None)
 
         hat_mu = stats.norm(loc=mu, scale=sigma).rvs()
-        data = np.array([(hat_mu,)], dtype=[('hat_mu', float)])
+        data = [np.array([(hat_mu,)], dtype=[('hat_mu', float)])]
         return data
 
-    def make_objective(self, guess=None, bound=None, minus=True, **kwargs):
-        if guess is None:
-            guess = {}
-        if bound is None:
-            bound = {}
-        names = []
-        bounds = []
-        guesses = []
-
-        for p in self._parameter_list:
-            if p not in kwargs:
-                g = guess.get(p, 1)
-                b = bound.get(p, None)
-                names.append(p)
-                guesses.append(g)
-                bounds.append(b)
-
-        sign = -1 if minus else 1
-
-        def cost(args):
-            # Get the arguments from args, then fill in the ones already fixed in outer kwargs
-            call_kwargs = {}
-            for i, k in enumerate(names):
-                call_kwargs[k] = args[i]
-            call_kwargs.update(kwargs)
-            return self.ll(**call_kwargs) * sign
-
-        return cost, names, np.array(guesses), bounds
-
-    def fit(self, guess=None, bound=None, verbose=False, **kwargs):
-        cost, names, guess, bounds = self.make_objective(minus=True, guess=guess,
-                                                         bound=bound, **kwargs)
-        minuit_dict = {}
-        for i, name in enumerate(names):
-            minuit_dict[name] = guess[i]
-
-        class MinuitWrap:
-            """Wrapper for functions to be called by Minuit
-
-            s_args must be a list of argument names of function f
-            the names in this list must be the same as the keys of
-            the dictionary passed to the Minuit call."""
-
-            def __init__(self, f, s_args):
-                self.func = f
-                self.s_args = s_args
-                self.func_code = make_func_code(s_args)
-
-            def __call__(self, *args):
-                return self.func(args)
-
-        # Make the Minuit object
-        cost.errordef = Minuit.LIKELIHOOD
-        m = Minuit(MinuitWrap(cost, names),
-                   **minuit_dict)
-        for k in self._fixed_parameters:
-            m.fixed[k] = True
-        for n, b in zip(names, bounds):
-            if b is not None:
-                print(n, b)
-                m.limits[n] = b
-
-        # Call migrad to do the actual minimization
-        m.migrad()
-        if verbose:
-            print(m)
-        return m.values.to_dict(), -1 * m.fval

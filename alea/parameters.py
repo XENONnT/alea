f96ad5b2d2a5995e2a1db7b199e5743faa225802
Robert Hammann <robert.hammann@outlook.com>
2023-06-20 09:57:39 -0400

    docs and reformat

--- alea/parameters.py
+++ alea/parameters.py
@@ -1,12 +1,37 @@
-from typing import Any
-import numpy as np  # noqa: F401
-import scipy.stats as stats  # noqa: F401
+from typing import Any, Dict, List, Optional, Tuple
+import numpy  # noqa: F401
+import scipy  # noqa: F401
 
 
 class Parameter:
-    def __init__(self, name, nominal_value=None, fittable=True,
-                 ptype=None, uncertainty=None, relative_uncertainty=None,
-                 blueice_anchors=None, fit_limits=None, description=None):
+    """
+    Represents a single parameter with its properties.
+
+    Attributes:
+        name (str): The name of the parameter.
+        nominal_value (float, optional): The nominal value of the parameter.
+        fittable (bool, optional): Indicates if the parameter is fittable or always fixed.
+        ptype (str, optional): The type of the parameter.
+        uncertainty (float or str, optional): The uncertainty of the parameter. If a string,
+            it can be evaluated as a numpy or scipy function to define non-gaussian constraints.
+        relative_uncertainty (bool, optional): Indicates if the uncertainty is relative to the nominal_value.
+        blueice_anchors (list, optional): Anchors for blueice template morphing.
+        fit_limits (tuple, optional): The limits for fitting the parameter.
+        description (str, optional): A description of the parameter.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        nominal_value: Optional[float] = None,
+        fittable: bool = True,
+        ptype: Optional[str] = None,
+        uncertainty: Optional[float or str] = None,
+        relative_uncertainty: Optional[bool] = None,
+        blueice_anchors: Optional[List] = None,
+        fit_limits: Optional[Tuple] = None,
+        description: Optional[str] = None,
+    ):
         self.name = name
         self.nominal_value = nominal_value
         self.fittable = fittable
@@ -18,27 +43,33 @@ class Parameter:
         self.description = description
 
     def __repr__(self) -> str:
-        parameter_str = [f"{k}={v}" for k, v in self.__dict__.items() if v is not None]
+        parameter_str = [f"{k}={v}" for k,
+                         v in self.__dict__.items() if v is not None]
         parameter_str = ", ".join(parameter_str)
         return f'{self.__class__.__module__}.{self.__class__.__qualname__}'\
             f'({parameter_str})'
 
     @property
-    def uncertainty(self):
+    def uncertainty(self) -> float or Any:
+        """
+        Returns the uncertainty of the parameter.
+        If the uncertainty is a string, it can be evaluated as a numpy or scipy function.
+        """
         if isinstance(self._uncertainty, str):
             # Evaluate the uncertainty if it's a string
-            if self._uncertainty.startswith("stats.") or self._uncertainty.startswith("np."):
+            if self._uncertainty.startswith("scipy.") or self._uncertainty.startswith("numpy."):
                 return eval(self._uncertainty)
             else:
-                raise ValueError(f"Uncertainty string '{self._uncertainty}' must start with 'stats.' or 'np.'")
+                raise ValueError(
+                    f"Uncertainty string '{self._uncertainty}' must start with 'scipy.' or 'numpy.'")
         else:
             return self._uncertainty
 
     @uncertainty.setter
-    def uncertainty(self, value):
+    def uncertainty(self, value: float or str) -> None:
         self._uncertainty = value
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
         """Returns True if all attributes are equal"""
         if isinstance(other, Parameter):
             return all(getattr(self, k) == getattr(other, k) for k in self.__dict__)
@@ -47,51 +78,116 @@ class Parameter:
 
 
 class Parameters:
+    """
+    Represents a collection of parameters.
+
+    Attributes:
+        parameters (dict): A dictionary to store the parameters, with parameter name as key.
+    """
+
     def __init__(self):
-        self.parameters = {}
+        self.parameters: Dict[str, Parameter] = {}
 
     @classmethod
-    def from_config(cls, config):
+    def from_config(cls, config: Dict[str, dict]):
+        """
+        Creates a Parameters object from a configuration dictionary.
+
+        Args:
+            config (dict): The configuration dictionary.
+
+        Returns:
+            Parameters: The created Parameters object.
+        """
         parameters = cls()
         for name, param_config in config.items():
             nominal_value = param_config.get("nominal_value", None)
             fittable = param_config.get("fittable", True)
             ptype = param_config.get("ptype", None)
             uncertainty = param_config.get("uncertainty", None)
-            relative_uncertainty = param_config.get("relative_uncertainty", None)
+            relative_uncertainty = param_config.get(
+                "relative_uncertainty", None)
             blueice_anchors = param_config.get("blueice_anchors", None)
             fit_limits = param_config.get("fit_limits", None)
             description = param_config.get("description", None)
-            parameter = Parameter(name, nominal_value, fittable,
-                                  ptype, uncertainty, relative_uncertainty,
-                                  blueice_anchors, fit_limits, description)
+            parameter = Parameter(
+                name,
+                nominal_value,
+                fittable,
+                ptype,
+                uncertainty,
+                relative_uncertainty,
+                blueice_anchors,
+                fit_limits,
+                description
+            )
             parameters.add_parameter(parameter)
         return parameters
 
-    def add_parameter(self, parameter):
+    def add_parameter(self, parameter: Parameter) -> None:
+        """
+        Adds a Parameter object to the Parameters collection.
+
+        Args:
+            parameter (Parameter): The Parameter object to add.
+        """
         self.parameters[parameter.name] = parameter
 
-    def get_parameter(self, name):
-        return self.parameters.get(name)
-
     @property
-    def names(self):
+    def names(self) -> List[str]:
+        """
+        Returns a list of parameter names.
+        """
         return list(self.parameters.keys())
 
-    def __call__(self, return_fittable=False, **kwargs: Any) -> dict:
+    def __call__(self, return_fittable: bool = False,
+                 **kwargs: Any) -> Dict[str, float]:
+        """
+        Returns a dictionary of parameter values, optionally filtered
+        to return only fittable parameters.
+
+        Args:
+            return_fittable (bool, optional): Indicates if only fittable parameters should be returned.
+            **kwargs: Additional keyword arguments to override parameter values.
+
+        Returns:
+            dict: A dictionary of parameter values.
+        """
         values = {}
+
         # check that all kwargs are valid parameter names
         for name in kwargs:
             if name not in self.parameters:
                 raise ValueError(f"Parameter '{name}' not found.")
+
         for name, param in self.parameters.items():
             new_val = kwargs.get(name, None)
             if (return_fittable and param.fittable) or not return_fittable:
                 values[name] = new_val if new_val is not None else param.nominal_value
         return values
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> Parameter:
+        """
+        Retrieves a Parameter object by attribute access.
+
+        Args:
+            name (str): The name of the parameter.
+
+        Returns:
+            Parameter: The retrieved Parameter object.
+
+        Raises:
+            AttributeError: If the attribute is not found.
+        """
         if name in self.parameters:
             return self.parameters[name]
         else:
             raise AttributeError(f"Attribute '{name}' not found.")
+
+    def __eq__(self, other: object) -> bool:
+        """Returns True if all parameters are equal"""
+        if isinstance(other, Parameters):
+            names = set(self.names + other.names)
+            return all(getattr(self, n) == getattr(other, n) for n in names)
+        else:
+            return False

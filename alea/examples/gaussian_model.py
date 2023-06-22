ccfa0f3094e9abb96f0add91520501ff39552917
Robert Hammann <robert.hammann@outlook.com>
2023-06-22 10:32:41 -0400

    draft example configs to think about structure

--- alea/examples/gaussian_model.py
+++ alea/examples/gaussian_model.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 class GaussianModel(StatisticalModel):
-    def __init__(self, parameter_definition: Optional[dict or list] = None):
+    def __init__(self, parameter_definition: Optional[dict or list] = None,
+                 **kwargs):
         """
         Initialise a model of a gaussian measurement (hatmu),
         where the model has parameters mu and sigma
@@ -14,7 +15,7 @@ class GaussianModel(StatisticalModel):
         """
         if parameter_definition is None:
             parameter_definition = ["mu", "sigma"]
-        super().__init__(parameter_definition=parameter_definition)
+        super().__init__(parameter_definition=parameter_definition, **kwargs)
 
     def _ll(self, mu=None, sigma=None):
         hat_mu = self.data[0]['hat_mu'][0]
---
+++ alea/statistical_models/blueice_extended_model.py
@@ -0,0 +1,17 @@
+from alea.statistical_model import StatisticalModel
+
+
+class BlueiceExtendedModel(StatisticalModel):
+    def __init__(self, parameter_definition: dict):
+        """
+        # TODO
+        """
+        super().__init__(parameter_definition=parameter_definition)
+
+    def _ll(self, **kwargs):
+        # TODO
+        pass
+
+    def _generate_data(self, **kwargs):
+        # TODO
+        pass

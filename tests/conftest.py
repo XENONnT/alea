5476dfefe3cce1595861647b8febfd078a594341
dachengx <dx2227@columbia.edu>
2023-07-27 11:01:29 -0500

    Add docstring

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,5 @@ import shutil
 
 @pytest.fixture(scope='class')
 def rm_cache():
+    """Remove pdf_cache directory before initializing the TestCase."""
     shutil.rmtree('pdf_cache', ignore_errors=True)

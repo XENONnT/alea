000b388848bd09e61a6e482aac5fc6e240de3041
dachengx <dx2227@columbia.edu>
2023-07-28 14:11:39 -0500

    Also delete the pdf_cache after tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,7 @@ import shutil
 
 @pytest.fixture(scope='class')
 def rm_cache():
-    """Remove pdf_cache directory before initializing the TestCase."""
+    """Remove pdf_cache directory before and after initializing the TestCase."""
+    shutil.rmtree('pdf_cache', ignore_errors=True)
+    yield
     shutil.rmtree('pdf_cache', ignore_errors=True)

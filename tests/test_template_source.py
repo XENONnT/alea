b3955f3cbade1b034105ce716641c806609909c2
dachengx <dx2227@columbia.edu>
2023-08-16 22:55:22 +0800

    Test template source separately in another test module

--- tests/test_template_source.py
+++ tests/test_template_source.py
@@ -1,10 +1,17 @@
 from unittest import TestCase
 
+from alea.models import BlueiceExtendedModel
+from alea.utils import load_yaml
+
 
 class TestTemplateSource(TestCase):
     """Test of the TemplateSource class."""
 
-    def test_load_templates(self):
-        # TODO: not sure whether we want to test TemplateSource in alea
-        # not sure TemplateSource.build_histogram and TemplateSource.simulate are called
-        pass
+    def test_init_templates(self):
+        """Test whether we can initialize template sources."""
+        parameter_definition = load_yaml("unbinned_wimp_statistical_model.yaml")[
+            "parameter_definition"
+        ]
+        likelihood_config = load_yaml("test_template_source.yaml")["likelihood_config"]
+        model = BlueiceExtendedModel(parameter_definition, likelihood_config)
+        model.nominal_expectation_values

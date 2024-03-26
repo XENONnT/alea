0012a75c05a578fed3e21bd6b054336cfcb8733e
Lanqing Yuan <yuanlq@uchicago.edu>
2024-03-26 00:07:12 -0500

    framework

--- alea/submitters/htcondor.py
+++ alea/submitters/htcondor.py
@@ -1,12 +1,32 @@
+# hyperparameters: configuration
+# inputs: templates in the specified local directory
+# outputs: all *.h5 files in the output folder
+
+
 from alea.submitter import Submitter
 
 
 class SubmitterHTCondor(Submitter):
     """Submitter for htcondor cluster.
-
-    Todo:
-        Implement this class.
-
     """
+    def __init__(self, *args, **kwargs):
+        self.name = self.__class__.__name__
+        self.htcondor_configurations = kwargs.get("slurm_configurations", {})
+        self.template_path = self.htcondor_configurations.pop("template_path", None)
+        super().__init__(*args, **kwargs)
+    
+    def _validate_x509_proxy(self):
+        raise NotImplementedError
+    
+    def _generate_sc(self):
+        raise NotImplementedError
+    
+    def _generate_workflow(self):
+        raise NotImplementedError
+    
+    def _plan_and_submit(self):
+        raise NotImplementedError  
 
-    pass
+    def submit_workflow(self):
+        raise NotImplementedError
+

96341be680578391364fdd4c32720cc8b1779b7f
Lanqing Yuan <yuanlq@uchicago.edu>
2024-03-26 01:37:33 -0500

    preparation

--- alea/submitters/htcondor.py
+++ alea/submitters/htcondor.py
@@ -1,7 +1,3 @@
-# hyperparameters: configuration
-# inputs: templates in the specified local directory
-# outputs: all *.h5 files in the output folder
-
 import subprocess
 import os
 import threading
@@ -9,23 +5,60 @@ import tempfile
 import time
 from alea.submitter import Submitter
 import logging
+from Pegasus.api import *
+from datetime import datetime
+
+DEFAULT_IMAGE = "/cvmfs/singularity.opensciencegrid.org/xenonnt/base-environment:latest"
+WORK_DIR = "/scratch/$USER/workflows"
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
 class SubmitterHTCondor(Submitter):
-    """Submitter for htcondor cluster.
+    """
+    Submitter for htcondor cluster.
     """
     def __init__(self, *args, **kwargs):
+        # General start
+        super().__init__(*args, **kwargs)
         self.name = self.__class__.__name__
         self.htcondor_configurations = kwargs.get("slurm_configurations", {})
+        self.singularity_image = self.htcondor_configurations.pop("singularity_image", DEFAULT_IMAGE)
+        self._initial_dir = os.getcwd()
+        self.work_dir = WORK_DIR
+        self._setup_wf_id()
+
+        # Job input configurations
         self.template_path = self.htcondor_configurations.pop("template_path", None)
+        
+        # Resources configurations
         self.request_cpus = self.htcondor_configurations.pop("request_cpus", 1)
         self.request_memory = self.htcondor_configurations.pop("request_memory", "2 GB")
         self.request_disk = self.htcondor_configurations.pop("request_disk", "2 GB")
-        super().__init__(*args, **kwargs)
-    
+        
+        # Dagman configurations
+        self.dagman_maxidle = self.htcondor_configurations.pop("dagman_maxidle", 100000)
+        self.dagman_retry = self.htcondor_configurations.pop("dagman_retry", 2)
+        self.dagman_maxjobs = self.htcondor_configurations.pop("dagman_maxjobs", 100000)
+
+
+    def _setup_wf_id(self):
+        """
+        Set up the workflow ID.
+        """
+        # If you have named the workflow, use that name. Otherwise, use the current time as name.
+        self._wf_id = self.htcondor_configurations.pop("wf_id")
+        if self._wf_id:
+            self.wf_id = self._wf_id
+        else:
+            self.wf_id = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+
     def _validate_x509_proxy(self, min_valid_hours=20):
-        """Ensure $HOME/user_cert exists and has enough time left
+        """
+        Ensure $HOME/user_cert exists and has enough time left. 
         """
         logger.debug('Verifying that the ~/user_cert proxy has enough lifetime')
         shell = Shell('grid-proxy-info -timeleft -file ~/user_cert')
@@ -35,30 +68,92 @@ class SubmitterHTCondor(Submitter):
             raise RuntimeError('User proxy is only valid for %d hours. Minimum required is %d hours.' \
                                %(valid_hours, min_valid_hours))
     
+
+    def _pegasus_properties(self):
+        """
+        Writes the file pegasus.properties. 
+        This file contains configuration settings used by Pegasus and HTCondor.
+        """
+        props = Properties()
+
+        # Don't ask why, but this is necessary for the Pegasus API to work
+        props["pegasus.metrics.app"] = "XENON"
+        props["pegasus.data.configuration"] = "nonsharedfs"
+
+        # Give jobs a total of 1+{retry} tries
+        props["dagman.retry"] = self.dagman_retry 
+        # Make sure we do start too many jobs at the same time
+        props["dagman.maxidle"] = self.dagman_maxidle
+        # Total number of jobs cap
+        props["dagman.maxjobs"] = self.dagman_maxjobs
+
+        # Help Pegasus developers by sharing performance data
+        props["pegasus.monitord.encoding"] = "json"
+        props["pegasus.catalog.workflow.amqp.url"] = "amqp://friend:donatedata@msgs.pegasus.isi.edu:5672/prod/workflows"
+
+        # write properties file to ./pegasus.properties
+        props.write()
+        self._pegasus_properties = props
+
+
     def _generate_sc(self):
-        raise NotImplementedError
+        sc = SiteCatalog()
+        
+        # Local site - this is the submit host
+        logger.debug("Defining local site")
+        local = Site("local")
+        # Logs and pegasus output goes here
+        scratch_dir = Directory(
+            Directory.SHARED_SCRATCH, path='{}/scratch/{}'.format(self.work_dir, self._wf_id)
+        )
+        scratch_dir.add_file_servers(
+            FileServer('file:///{}/scratch/{}'.format(self.work_dir, self.wf_id), Operation.ALL)
+        )
+        # Jobs outputs goes here
+        storage_dir = Directory(
+            Directory.LOCAL_STORAGE, path='{}/outputs/{}'.format(self.work_dir, self.wf_id)
+        )
+        storage_dir.add_file_servers(
+            FileServer('file:///{}/outputs/{}'.format(self.work_dir, self.wf_id), Operation.ALL)
+        )
+        # Add scratch and storage directories to the local site
+        local.add_directories(scratch_dir, storage_dir)
+
+
+
     
+
     def _generate_tc(self):
         raise NotImplementedError
     
+
     def _generate_rc(self):
         raise NotImplementedError
 
+
     def _generate_workflow(self):
-        raise NotImplementedError
+        self.wf = Workflow('alea')
+        self.sc = self._generate_sc()
+        self.tc = self._generate_tc()
+        self.rc = self._generate_rc()
+
     
     def _plan_and_submit(self):
         raise NotImplementedError  
 
+
     def submit_workflow(self):
-        raise NotImplementedError
+        self._pegasus_properties()
+
+        # Return to initial dir, as we are done.
+        logger.info('We are done. Returning to initial directory.')
+        os.chdir(self._initial_dir)
 
 
 class Shell(object):
-    '''
+    """
     Provides a shell callout with buffered stdout/stderr, error handling and timeout
-    '''
-        
+    """ 
     def __init__(self, cmd, timeout_secs = 1*60*60, log_cmd = False, log_outerr = False):
         self._cmd = cmd
         self._timeout_secs = timeout_secs
@@ -124,19 +219,19 @@ class Shell(object):
 
     def get_outerr(self):
         """
-        returns the combined stdout and stderr from the command
+        Returns the combined stdout and stderr from the command
         """
         return self._outerr
     
     
     def get_exit_code(self):
         """
-        returns the exit code from the process
+        Returns the exit code from the process
         """
         return self._process.returncode
     
     def get_duration(self):
         """
-        returns the timing of the command (seconds)
+        Returns the timing of the command (seconds)
         """
         return self._duration

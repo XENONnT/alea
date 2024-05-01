002fafaedc3d32fbfd904ff2f90ba12a08d040e4
yuema137 <3124558229@qq.com>
2024-05-01 17:53:19 -0500

    modify length check for bins

--- alea/ces_transformation.py
+++ alea/ces_transformation.py
@@ -37,7 +37,7 @@ def smearing_mono_gaussian(
     else:
         # use the bins that set by the user
         bins = np.array(bins)
-        if len(bins) <= 1:
+        if bins.size <= 1:
             raise ValueError("bins must have at least 2 elements")
         bin_centers = 0.5 * (bins[1:] + bins[:-1])
         data = stats.norm.pdf(

6355284ffa38044145ab85df9b615cf00b4a2c9b
Lanqing Yuan <yuanlq@uchicago.edu>
2024-05-09 16:36:48 -0500

    copy x509

--- alea/submitters/setup_pegasus.sh
+++ alea/submitters/setup_pegasus.sh
@@ -3,7 +3,10 @@
 
 . /cvmfs/xenon.opensciencegrid.org/releases/nT/development/setup.sh
 
-export X509_USER_PROXY=/ospool/uc-shared/project/xenon/grid_proxy/xenon_service_proxy
+cp /ospool/uc-shared/project/xenon/grid_proxy/xenon_service_proxy $HOME/.xenon_service_proxy
+chmod 600 $HOME/.xenon_service_proxy
+
+export X509_USER_PROXY=$HOME/.xenon_service_proxy
 export PATH=/opt/pegasus/current/bin:$PATH
 export PYTHONPATH=`pegasus-config --python`:$PYTHONPATH
 export PYTHONPATH="$HOME/.local/lib/python3.9/site-packages${PYTHONPATH:+:$PYTHONPATH}"

beee513fd62b39883724e8c6b6d2c2f4d9a475cb
yuema137 <3124558229@qq.com>
2025-02-20 13:17:22 -0600

    set default fiducial mass to 1

--- alea/models/blueice_extended_model.py
+++ alea/models/blueice_extended_model.py
@@ -268,9 +268,11 @@ class BlueiceExtendedModel(StatisticalModel):
         pdf_base_config["livetime_days"] = self.parameters[
             pdf_base_config["livetime_parameter"]
         ].nominal_value
-        pdf_base_config["fiducial_mass"] = self.parameters[
-            pdf_base_config["fiducial_mass_parameter"]
-        ].nominal_value
+        pdf_base_config["fiducial_mass"] = (
+            self.parameters[pdf_base_config["fiducial_mass_parameter"]].nominal_value 
+            if "fiducial_mass_parameter" in pdf_base_config 
+            else 1
+        )
         for p in self.parameters:
             # adding the nominal rate values will screw things up in blueice!
             # So here we're just adding the nominal values of all other parameters
